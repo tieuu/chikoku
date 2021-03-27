2396f29e7aafdfb0eb56b6c05ed2840be4a70374
Julian Segrestan <julian.nihon@gmail.com>
2021-03-27 23:59:23 +0900

    corrected model

--- db/migrate/20210327145849_create_time_slots.rb
+++ db/migrate/20210327145849_create_time_slots.rb
@@ -2,7 +2,7 @@ class CreateTimeSlots < ActiveRecord::Migration[6.0]
   def change
     create_table :time_slots do |t|
       t.references :calendar, null: false, foreign_key: true
-      t.dateTime :start
+      t.datetime :start
       t.time :duration
 
       t.timestamps
