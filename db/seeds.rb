9ce5ac975d3adb3c9cbdea0a45d9bd8d55434c5b
tieu <68626023+tieuu@users.noreply.github.com>
2021-05-10 22:57:06 +0900

    made a bit of seed but migration didnt work
    good luck julian

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "deleting database"
+User.destroy_all
+
+
+julian = User.create!(username: 'julian', leader: false, email: "julian@gmail.com", password: "julian")
+tieu = User.create!(username: 'tieu', leader: true, email: "tieu@gmail.com", password: "tieu")
+
+big_calendar = Calendar.create!(user: tieu)
+
+time_slots = TimeSlot.create!(calendar: big_calendar, start: )
